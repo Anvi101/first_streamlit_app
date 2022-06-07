6f324c05a5aa02ef1729c62785dbdf7957b68c28
Anvi101 <104450255+Anvi101@users.noreply.github.com>
2022-06-06 23:26:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
 
 import streamlit
+import pandas
+
 streamlit.header('My Mom\'s New Healthy Diner')
 streamlit.text('BREAKFAST MENU')
 streamlit.text('🥣 OMEGA3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinace & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free Range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
 
