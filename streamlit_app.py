54ff3cdef4491419a34d5d40f207775d9245e5da
Anvi101 <104450255+Anvi101@users.noreply.github.com>
2022-06-06 23:34:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list =my_fruit_list.set_index("Fruits")
+my_fruit_list =my_fruit_list.set_index('Fruit')
+
 streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so that they can select the fruits they like
