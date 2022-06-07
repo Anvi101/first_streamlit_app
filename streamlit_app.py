bf0ed5384df5ddf02d54bb35de6744e46925746d
Anvi101 <104450255+Anvi101@users.noreply.github.com>
2022-06-06 23:17:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('BREAKFAST MENU')
+streamlit.title('OMEGA3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinace & Rocket Smoothie')
+streamlit.title('Hard Boiled Free Range Eggs')
