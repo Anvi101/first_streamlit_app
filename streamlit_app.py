89927f48882baca5216dc9d2d55589f3777078a7
Anvi101 <104450255+Anvi101@users.noreply.github.com>
2022-06-06 23:19:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.text('BREAKFAST MENU')
 streamlit.text('OMEGA3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinace & Rocket Smoothie')
 streamlit.text('Hard Boiled Free Range Eggs')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
