bc980b4d873df588f9646823e61e3b187898d8ae
Anvi101 <104450255+Anvi101@users.noreply.github.com>
2022-06-16 15:17:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,6 +59,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows =get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
+streamlit.stop()
 
 add_my_fruit =streamlit.text_input('What fruit would you like to add?','Kiwi')
 #fruityvice_response_add = requests.put("https://fruityvice.com/api/fruit/"+add_my_fruit)
