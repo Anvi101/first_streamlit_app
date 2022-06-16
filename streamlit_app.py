90ab846c4684d2c1597a6bfa0253a0b2e86959f4
Anvi101 <104450255+Anvi101@users.noreply.github.com>
2022-06-15 20:35:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,10 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice\'s fruit advice')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice =streamlit.text_input('What fruit you would like information about?','Kiwi')
+streamlit.write('User entered choice',fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
 fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalize)
